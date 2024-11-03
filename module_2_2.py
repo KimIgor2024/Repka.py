bdc57bdcfea296b6ccefb3b64adbf51c907973b7
KimIgor2024 <kim317267@gmail.com>
2024-11-03 16:32:29 +0300

    Add files via upload

---
+++ module_2_2.py
@@ -0,0 +1,10 @@
+first = input( "Введите число First:" )
+second = input("Введите число Sekond:" )
+third = input( "Введите число Third:")
+print(first == second == third)
+if first == second == third:
+    print("если все числа равны , равно 3")
+elif first  == second  or first == third or third == second:
+    print("если два числа равны ,равно 2")
+elif first != second != third:
+    print(" если числа не равны ,равно 0 " )123
