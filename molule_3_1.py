5d566bf1e41d3cee7593edf36a61064aa21baf7c
KimIgor2024 <kim317267@gmail.com>
2024-11-10 14:20:01 +0300

    Add files via upload

---
+++ molule_3_1.py
@@ -0,0 +1,28 @@
+
+#  "Цель: применить на практике начальные знания о пространстве имён и оператор global. Закрепить навыки из предыдущих модулей."
+
+calls = 0
+
+def count_calls():
+    global calls
+    calls += 1
+
+def string_info(string):
+    count_calls()
+    return len(string), string.upper(), string. lower()
+
+def is_contains(string, list_to_search):
+    count_calls()
+    string = string.lower()
+    for item in list_to_search:
+        if item . lower() == string:
+            return True
+    return False
+
+
+print(string_info('Capybara'))
+print(string_info('Armageddon'))
+print(is_contains('Urban',['ban','BaNaN ','urBAN']))
+print(is_contains('cycle',['recycling', 'cyclic']))
+print(calls)
+
